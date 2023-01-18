35a9549e6239d56d2e2b8dc0ece6ed78602dcf60
NganPham89 <algarvian2001@yahoo.com>
2023-01-18 15:50:42 -0500

    added more scripts and gura2 pic

--- Practice/scripts/main.js
+++ Practice/scripts/main.js
@@ -1,4 +1,34 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+const myImage = document.querySelector("img");
 
-let myVariable = "Bob";
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/gura.png") {
+        myImage.setAttribute("src", "images/gura2.png");
+    } else {
+        myImage.setAttribute("src", "images/gura.png");
+    }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    let myName = prompt("Please enter your name.");
+    if (!myName) {
+        setUserName();
+    } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = 'Gura is cute, ' + myName;
+    }
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem("name");
+    myHeading.textContent = 'Gura is cute, ' + storedName;
+}
+
+myButton.onclick = () => {
+    setUserName();
+};
